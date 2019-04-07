1c7d93ebc3627be65e0761cd600fc7d10afb2175
ritik027 <ritikmalik503@gmail.com>
2019-04-07 21:17:38 +0530

    added exe file a1 and made changes

--- index1.c
+++ index1.c
@@ -1,21 +1,33 @@
 #include<stdio.h>
-int main()
-{
-	int no_of_processes;
-	
-	printf("Enter the no of processes you want in the device queue\n");
-	no_of_processes = scanf("%d",&no_of_processes);
-	
-	int arrival_time[no_of_processes];
-	int burst_time[no_of_processes];
 
+int no_of_processes;
+
+struct Process
+{
+	int arrival_time;
+	int burst_time;
+	int process_id;
+	int waiting_time;
+	int turnaround_time;
+};
+
+void input(struct Process all_processes[])
+{
 	for(int i = 0; i < no_of_processes; i++)
 	{
 		printf("Enter Arrival time for Process %d\n", (i + 1));
-		scanf("%d",&arrival_time[i]);
+		scanf("%d",&all_processes[i].arrival_time);
 		printf("Enter Burst time for Process %d\n", (i + 1));
-		scanf("%d",&burst_time[i]);
+		scanf("%d",&all_processes[i].burst_time);
+		all_processes[i].process_id = i + 1;
 	}
-
+}
+int main()
+{
 	
+	printf("Enter the no of processes you want in the device queue\n");
+	scanf("%d",&no_of_processes);
+	
+	struct Process all_processes[no_of_processes];
+	input(all_processes);
 }
