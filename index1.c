420d925c4e60c07252adcf6ef9b21b38bdf5a3e1
ritik027 <ritikmalik503@gmail.com>
2019-04-09 22:36:58 +0530

    made changes

--- index1.c
+++ index1.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+#include<stdbool.h>
 int no_of_processes;
 
 struct Process
@@ -10,7 +10,25 @@ struct Process
 	int waiting_time;
 	int turnaround_time;
 };
-
+bool cmp(struct Process x, struct Process y)
+{
+	return x.arrival_time > y.arrival_time;
+}
+void sort(struct Process all_processes[])
+{
+	for(int i = 0; i < no_of_processes - 1; i++)
+	{
+		for(int j = 0; j < no_of_processes - i - 1; j++)
+		{
+			if(cmp(all_processes[j], all_processes[j + 1]))
+			{
+				struct Process temp = all_processes[j];
+				all_processes[j] = all_processes[j + 1];
+				all_processes[j + 1] = temp;
+			}
+		}
+	}
+}
 void input(struct Process all_processes[])
 {
 	for(int i = 0; i < no_of_processes; i++)
@@ -22,6 +40,13 @@ void input(struct Process all_processes[])
 		all_processes[i].process_id = i + 1;
 	}
 }
+void print(struct Process all_processes[])
+{
+	for(int i = 0; i < no_of_processes; i++)
+	{
+		printf("pid - %d      AT - %d\n", all_processes[i].process_id, all_processes[i].arrival_time);
+	}
+}
 int main()
 {
 	
@@ -30,4 +55,6 @@ int main()
 	
 	struct Process all_processes[no_of_processes];
 	input(all_processes);
+	sort(all_processes);
+	print(all_processes);
 }
